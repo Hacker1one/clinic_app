9d6f37a8f2eede7f252825847cdc4a1da6538911
n0rbed <yassinahmad789@gmail.com>
2024-12-07 14:09:22 +0200

    documentupload

---
+++ webclinic/Pages/DocumentUpload.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace webclinic.Pages
+{
+    public class DocumentUploadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
